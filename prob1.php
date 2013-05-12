23c53efe024310f6bbbb93a197a5750482d4b265
lndl <laudleon@gmail.com>
2013-05-12 15:12:36 +0000

    second commit

---
+++ prob1.php
@@ -0,0 +1,8 @@
+<?php
+
+$multiplos3 = range(0,1000-1,3);
+$multiplos5 = range(0,1000-1,5);
+$respuesta = array_sum(array_unique(array_merge($multiplos3, $multiplos5)));
+echo "Respuesta del ejercicio 1: ".$respuesta."\n";
+
+?>
---
+++ prob6.php
@@ -0,0 +1,25 @@
+<?php
+
+//Calcula el cuadrado de un numero
+function square($x) {return $x*$x;}
+
+/* 
+
+//Calcula la suma de los elementos numericos de un arreglo
+function array_reduce_sum($arrayNum) {
+  return array_reduce($arrayNum, function($acc, $i) {
+    $acc += $i;
+    return $acc;  
+  }, 0);
+}
+
+*/
+
+//Rango de numeros naturales (de 1 a 100)
+$nums = range(1,100);
+
+//Calculo de la respuesta del ejercicio
+$respuesta = square(array_sum($nums)) - array_sum(array_map("square", $nums));
+echo "Respuesta del ejercicio 6: ".$respuesta."\n";
+
+?>
