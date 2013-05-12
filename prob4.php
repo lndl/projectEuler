45f78c0d46898a2bcc7f38209176295594b79d00
lndl <laudleon@gmail.com>
2013-05-12 15:50:00 +0000

    --

--- prob4.php
+++ prob4.php
@@ -2,19 +2,18 @@
 
 //Ejercicio 4
 
-$l = array();
+$maxM = -1;
 for ($x=100; $x<=999; $x++) {
   for ($y=100; $y<=999; $y++) {
     if ($x <= $y) { //Evitar multiplicaciones innecesarias (mult. conmutativa)
       $m = $x*$y;
       if ((string)$m == strrev(((string)$m))) //Verificar si el producto es capicua
-        $l[] = $m;
+        if ($m > $maxM) //Se guarda la multiplicacion capicua mas grande
+          $maxM = $m;
     }
   }
 }
 
-//se ordena el arreglo de menor a mayor, se obtiene el ultimo elemento (el mas grande)
-asort($l);
-echo "Respuesta del problema 4: ".end($l)."\n";
+echo "Respuesta del problema 4: ".$maxM."\n";
 
 ?>
